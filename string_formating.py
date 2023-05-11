22e15717612e7def501ba733ebedd6881e6a8139
GamalHisham <gamalhisham506@gmail.com>
2023-05-11 16:33:01 +0200

    finish string formating

--- string_formating.py
+++ string_formating.py
@@ -1,13 +1,12 @@
-
-#______________________________
-#___string formating old way __
-#______________________________
+# ______________________________
+# ___string formating old way __
+# ______________________________
 
 # Name="gamal"
 # l="python"
 # Ex=10
 
-# print("My name is " + Name + " I am " + l + " developer " ) #concatenate 
+# print("My name is " + Name + " I am " + l + " developer " ) #concatenate
 
 # print("My name is %s I am %s developer with %d Years Experience" % (Name,l,Ex)) #place holder
 
@@ -27,10 +26,9 @@
 # print("My age is %.5s" % Name)
 
 
-
-#______________________________
-#___string formating new way __
-#______________________________
+# ______________________________
+# ___string formating new way __
+# ______________________________
 
 # Name="gamal"
 # l="python"
@@ -50,6 +48,30 @@
 
 # control string
 
-Name="gamal hisham saad"
-print("My age is {:s}".format(Name))
-print("My age is {:.5s}".format(Name))
+# Name="gamal hisham saad"
+# print("My age is {:s}".format(Name))
+# print("My age is {:.5s}".format(Name))
+
+# format money
+# money = 5556789345984
+# print("My money in bank {}".format(money))
+# print("My money in bank {:_}".format(money))
+# print("My money in bank {:_d}".format(money))
+
+# Rearrange indexing
+# a, b, c = "one", "two", "three"
+# print("Arrange is : {} {} {}".format(a, b, c))
+# print("Arrange is : {1} {2} {0}".format(a, b, c)) #rearrange
+
+# x, y, z = 10, 20, 30
+# print("Arrange is : {} {} {}".format(x, y, z))
+# print("Arrange is : {1} {2} {0}".format(x, y, z))
+# print("Arrange is : {1:f} {2:f} {0:f}".format(x, y, z))
+# print("Arrange is : {1:.2f} {2:.4f} {0:.6f}".format(x, y, z))
+
+# format in version 3.6+
+
+# name = "gamal"
+# age = 26
+# print("My name is {name} and My age is {age}")
+# print(f"My name is {name} and My age is {age}")
