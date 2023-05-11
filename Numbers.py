c2130eb035d60fd0a32c9fe1048b4a9bf658a806
GamalHisham <gamalhisham506@gmail.com>
2023-05-11 17:37:13 +0200

    Numbers and Arthmetic operatos

---
+++ Numbers.py
@@ -0,0 +1,70 @@
+# _______________________________
+# _________Numbers_______________
+# _______________________________
+
+# integer
+# print(type(10))
+# print(type(0))
+# print(type(-123))
+# print(type(300))
+
+# # float
+# print(type(10.23))
+# print(type(-12.34))
+# print(type(-123.1))
+# print(type(0.23))
+
+# # complex
+# x = 4 + 8j
+# print(type(x))
+# print("Real part {}".format(x.real))
+# print("Imag part {}".format(x.imag))
+
+# Type casting
+
+# print(type(int(10.23)))
+# print(type(int(-12.34)))
+# print(type(float(12)))
+# print(type(complex(5)))
+# print(type(complex(3.5)))
+# print(type(int(3 + 4j)))  # Error
+
+
+# ___________________________
+# ____Arthmetic operators____
+# ___________________________
+
+# Addition
+print(10 + 23)
+print(-10 + 23)
+print(10 + -23)
+
+# Subtraction
+print(10 - 23)
+print(-10 - 23)
+print(10 - -23)
+
+# Multiplication
+print(10 * 23)
+print(-10 * 23)
+print(10 * 10)
+
+# Division
+print(20 / 5)
+print(-10 / -2)
+print(10 / 10)
+
+# Modulus
+print(20 % 5)
+print(22 % 5)
+print(23 % 5)
+
+# Floor division
+print(20 // 5)
+print(22 // 5)
+print(23 // 5)
+
+# Exponential
+print(2**3)
+print(2**2)
+print(2**5)
