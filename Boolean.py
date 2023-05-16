27cf6dff8c0d4affb82c92ddc8b7fea3b33195a9
gamal hisham <gamalhisham506@gmail.com>
2023-05-16 11:29:11 +0300

    Boolean and Assign operators

---
+++ Boolean.py
@@ -0,0 +1,68 @@
+
+# ______________________________
+# __________Boolean_____________
+# 1- In programming you need to know output is True or False 
+# 2- Boolean values are two constant objects (TRUE OR FALSE)
+# ______________________________
+
+name=" "
+# print(name.isspace())
+
+# print("=" * 50)
+
+# true values
+# print(bool(100))
+# print(bool("gamal"))
+# print(bool([1,2,3]))
+# print(bool((1,2,3)))
+# print(bool({2,3,4}))
+# print(bool({"one":1}))
+# print(bool(True))
+
+print("=" * 50)
+
+# false values 
+# print(bool(0))
+# print(bool(""))
+# print(bool([]))
+# print(bool(()))
+# print(bool({}))
+# print(bool(None))
+# print(bool(False))
+
+
+# ______________________________
+# ______Boolean Operators_______
+# 1- AND 
+# 2- OR
+# 3- NOT
+# ______________________________
+
+
+age=26
+countary="Egypt"
+Rank=10
+
+# and
+# print(age>20 and countary=="Egypt" and Rank==5)
+# print(age>20 and countary=="Egypt" and Rank>5)
+
+# or 
+# print(age>20 or countary=="Egypt" or Rank==5)
+# print(age>20 or countary=="Egypt" or Rank==5)
+
+# not (reverse logic value)
+# print(not age>20)
+
+
+
+
+
+
+
+
+
+
+
+
+
