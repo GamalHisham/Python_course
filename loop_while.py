4ead4a255cab155637643e1c1ab3def457c67c53
GamalHisham <gamalhisham506@gmail.com>
2023-05-21 23:20:19 +0200

    Examples while loop

--- loop_while.py
+++ loop_while.py
@@ -1,9 +1,8 @@
-
-#------------------------
-#----While loop ---------
-#while(condition is true):
+# ------------------------
+# ----While loop ---------
+# while(condition is true):
 #     code run
-#------------------------
+# ------------------------
 
 # a=0
 
@@ -15,17 +14,50 @@
 #     print("while finished") # Else will run when while condition is false
 
 
+# friends=["gamal","saad","alaa","mohamed","eltyeb","hisham","saber","mostafa","hisham","saber"]
 
-friends=["gamal","saad","alaa","mohamed","eltyeb","hisham","saber","mostafa","hisham","saber"]
+# x=0
 
-x=0
+# while x < len(friends):
 
-while x < len(friends):
+#     print(f"# {str(x+1).zfill(2)} {friends[x]}")
 
-    print(f"# {str(x+1).zfill(2)} {friends[x]}")
+#     x += 1
 
-    x += 1
+# else:
+#     print("all friends printed")
 
+
+# ------------------------
+# ----Bookmark Manage-----
+# ------------------------
+
+# Empty list for websites
+websites = []
+
+# maximum added websites
+maxwebsites = 3
+
+while maxwebsites > 0:
+    # input website
+    addwebsites = input('please enter website without "https:" :').strip().lower()
+    websites.append(f"https//{addwebsites}")
+
+    maxwebsites -= 1
+
+    print(websites)
 else:
-    print("all friends printed")
+    print("list are full")
 
+print("=" * 50)
+
+
+# print website list element
+
+if len(websites) > 0:
+    websites.sort()
+    index = 0
+
+    while index < len(websites):
+        print(websites[index])
+        index += 1
