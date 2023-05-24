888703413c90448acd6e4fd16e5b2284aded485f
gamal hisham <gamalhisham506@gmail.com>
2023-05-24 15:37:57 +0300

    functions

---
+++ functions.py
@@ -0,0 +1,77 @@
+
+# ______________________________
+# ___________Functions__________
+# 1- A function is a block of code do a task 
+# 2- A function run when you call it 
+# 3- A function accept element to deal with called [parameters]
+# 4- A function can do task without returning data
+# 5- A function can return data after JOb is finished
+# 6- A function creat to prevent dry
+# 7- A function accept element when you call it  called [Arguments]
+# 8- There's a built_in functions and user defined functions
+# 9- A function is all for team and all apps
+# ______________________________
+
+
+
+# def print_name (): #A function can do task without returning data
+
+#     print("gamal hisham saad")
+
+# print_name()
+
+
+# def print_name (): #A function can return data after JOb is finished
+
+#     name = "gamal hisham saad"
+#     return name
+
+# print(print_name())
+
+#-----------------------------------------------------------------------------------------------
+
+# ______________________________
+# ____parameters and arguments__
+#_______________________________
+
+# def                            => function keyword
+# say_hello                      => function name
+# name                           => function parameter
+# print(f"hello {name}")         => function task
+# say_hello("gamal")             => gamal is  argument
+
+
+
+# def say_hello (name):
+
+#     print(f"hello {name}")
+
+
+# say_hello("gamal")
+
+#-----------------------------------------------------------
+
+# def addition (n1,n2):
+
+#     if type(n1) != int or type(n2) != int :
+#         print ("only add integers numbers")
+
+#     else:
+#         print(n1+n2)
+
+# addition(100,300)
+
+# addition("100",300)
+
+#---------------------------------------------------------------
+
+
+def full_name (f,m,l):
+
+    print(f" hello {f.strip().capitalize()} {m.upper():.1s} {l.capitalize()}")
+
+
+# full_name("  gamal   ","hisham" ,"saad")
+
+
+
