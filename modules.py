c04be5154a6be13ffed97107ffcf67b60dab0d65
GamalHisham <gamalhisham506@gmail.com>
2023-06-10 18:20:41 +0200

    Modules

---
+++ modules.py
@@ -0,0 +1,27 @@
+# ----------------------------------
+# ----Modules => Built in modules---
+# [1] module is a file contain a set of fun
+# [2] can import module to help you
+# [3] can import  Multiple module
+# [4] can creat your own modules
+# [5] modules save your time
+# ----------------------------------
+
+import random  # will call all fun from module
+
+# print(random)
+
+print(f"random float num {random.random()}")
+
+print("=" * 50)
+
+# show all fun in module
+# print(dir(random))
+
+print("=" * 50)
+
+# from random import randint, random  # will call one fun from module
+
+# print(f"random float num {random()}")
+
+# print(f"random int {randint(10,20)}")
